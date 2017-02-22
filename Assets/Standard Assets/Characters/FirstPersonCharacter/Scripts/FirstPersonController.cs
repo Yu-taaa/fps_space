b310c992e9e4858002719015ef7f8bfcb6a1c521
konndoyuta <konndoyuta@konndoyuta-no-MacBook-Pro.local>
2017-02-23 00:06:56 +0900

    miss

--- Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -81,6 +81,13 @@ namespace UnityStandardAssets.Characters.FirstPerson
             }
 
             m_PreviouslyGrounded = m_CharacterController.isGrounded;
+
+			//しゃがむ機能の実装
+//			if (Input.GetKey(KeyCode.C))
+//            {
+//				transform.position.y =
+//             }
+				
         }
 
 
