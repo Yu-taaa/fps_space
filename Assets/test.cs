4a70a2b0d649c2be30558b4b2b9c2d43d54bbb85
konndoyuta <konndoyuta@konndoyuta-no-MacBook-Pro.local>
2017-02-23 01:15:56 +0900

    create a simple demo sean

---
+++ Assets/test.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update () {
+
+	    if (Input.GetKey(KeyCode.C))
+	    {
+	        transform.position = new Vector3(transform.position.x, 30f, transform.position.z);
+	    }
+		
+	}
+}
