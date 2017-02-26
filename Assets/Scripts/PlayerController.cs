222540922944147ef10211a291210e3a49d7131d
konndoyuta <konndoyuta@konndoyuta-no-MacBook-Pro.local>
2017-02-27 04:19:00 +0900

    add PlayerController.cs to coolTime

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,41 +4,44 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public GameObject targetObj;
-    AudioSource audioSource;
+    [SerializeField] AudioSource _audioSource;
+    [SerializeField] private GameObject _target;
+    [SerializeField] private int _bullets = 30;
+    [SerializeField] private int _bulletsBox = 150;
+    private float coolTime = 0.0f;
+
+    private GameObject _Spark;
 
     // Use this for initialization
     void Start()
     {
-        audioSource = gameObject.GetComponent<AudioSource>();
+        _audioSource = gameObject.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        coolTime += Time.deltaTime;
 
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") && coolTime > 0.5f)
         {
-            //fire();
-            if (Input.GetButtonDown("Fire1")) {
-                audioSource.Play();
-            }
+            _audioSource.Play();
+            fire();
         }
     }
 
     void fire()
     {
-        // 画面中央の座標を取得。
-        Vector3 point = new Vector3(Screen.width / 2, Screen.height / 2, 0);
-        Ray ray = Camera.main.ScreenPointToRay(point);
+        Ray ray = new Ray(transform.position, transform.forward);
         RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
+        if (!Physics.Raycast(ray, out hit))
         {
-            targetObj = hit.collider.gameObject;
+            return;
         }
-        else
-        {
-            targetObj = null;
-        }
+
+        _Spark = (GameObject) Instantiate(_target);
+        _Spark.transform.position = hit.point;
+        Destroy(_Spark, 0.3f);
+        coolTime = 0.0f;
     }
 }
