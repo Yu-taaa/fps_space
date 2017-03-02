4f4b7144fb32e64c1f9b6f37a39480535b505e46
konndoyuta <konndoyuta@konndoyuta-no-MacBook-Pro.local>
2017-03-02 18:47:20 +0900

    add Target.cs and Target to animation funciton

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -2,26 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Target : MonoBehaviour {
-
+public class Target : MonoBehaviour
+{
+    private Animator _anim;
     [SerializeField] int _hp = 5;
 
 
-
     // Use this for initialization
-	void Start () {
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
 
-	}
-
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    void Update()
+    {
+    }
 
 
     public void HitTarget()
     {
-        print(_hp);
-        _hp--;
+        if (_hp > 0)
+        {
+            print(_hp);
+            _hp--;
+        }
+        else if (_hp == 0)
+        {
+            _anim.SetBool("IsFall", true);
+            Invoke("UpTarget", 3.0f);
+        }
+    }
+
+
+    void UpTarget()
+    {
+        _anim.SetBool("IsFall", false);
+        _hp = 5;
     }
 }
