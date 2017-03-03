5e14b1c14364bd52f4098299859c3ad85b3ce9f3
konndoyuta <konndoyuta@konndoyuta-no-MacBook-Pro.local>
2017-03-03 20:52:11 +0900

    add Ui.cs to text

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -4,10 +4,11 @@ using System.Diagnostics;
 using System.Runtime.Serialization.Formatters;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
-    private int score = 0;
+    public int _scorePoint;
     private float _distance;
     private Vector3 _headMarkerPosition;
     [SerializeField] private GameObject _headMarker;
@@ -16,13 +17,13 @@ public class Score : MonoBehaviour
     public void ScoreCount(Vector3 bulletHitPoint) //rayのあたった位置
     {
         _headMarkerPosition = _headMarker.transform.position;
-        _distance = Vector3.Distance(bulletHitPoint, _headMarkerPosition);
-        Count(_distance);
-        score += Count(_distance);
-        print(score);
+        _distance = Vector2.Distance(bulletHitPoint, _headMarkerPosition);
+        // Count(_distance);
+        _scorePoint = _scorePoint + Count(_distance);
+        //print(_scorePoint);
     }
 
-     private int Count(float distance)
+    private int Count(float distance)
     {
         if (distance < 0.1f)
         {
--- Assets/Scripts/Ui.cs
+++ Assets/Scripts/Ui.cs
@@ -1,16 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Ui : MonoBehaviour {
+public class Ui : MonoBehaviour
+{
+    private float _time;
+    [SerializeField] private GunController GunController;
+    [SerializeField] private Text _timeText;
+    [SerializeField] private Text _ptText;
+    [SerializeField] private Text _bulletboxText;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField] private Text _bulletText;
+
+    // [SerializeField] private Score _headMarker;
+    [SerializeField] private Score _score;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        _time = 90.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _time -= Time.deltaTime;
+        _timeText.text = "Time : " + _time.ToString("N1") + "s";
+        _ptText.text = "Pt : " + _score._scorePoint;
+        _bulletboxText.text = "BulletsBox : " + GunController.BulletsBox;
+        _bulletText.text = "Bullets : " + GunController.Bullets;
+    }
 }
